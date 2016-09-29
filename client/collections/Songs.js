4e8d537520595057790c214389fc5f6126d43bcd
bruceisyoung <isyoung.bruce@gmail.com>
2016-09-29 15:41:02 -0700

    finish the prompts of songs
    -[x] fetch song data from parse

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,24 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
 
+
+//original songs is just one song
+  initialize: function() {
+    var songReq = new XMLHttpRequest();
+    songReq.open('GET', 'https://api.parse.com/1/classes/songs/', true);
+
+    var songLoadedCount = 0;
+    songReq.onload = (function () {
+      results = JSON.parse(songReq.responseText).results;
+      
+      if (songLoadedCount < results.length ) {
+        this.add(results[songLoadedCount]);
+        songLoadedCount++;
+      }
+    }).bind(this);
+
+    
+  }
 });
